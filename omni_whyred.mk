9c65330dba9b48107523bc1062264b21ccfe2405
maxwen <max.weninger@gmail.com>
2018-07-22 11:18:51 +0200

    whyred: always use DEVICE_PACKAGE_OVERLAYS
     - Also moved device overlay into omni mk
    
    Change-Id: Ie103971e83672c64a21eb85c600619113db7859e

--- omni_whyred.mk
+++ omni_whyred.mk
@@ -23,12 +23,12 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+DEVICE_PACKAGE_OVERLAYS += device/xiaomi/whyred/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+
 # Inherit from Omni custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
-
-# CarrierConfig
-PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
