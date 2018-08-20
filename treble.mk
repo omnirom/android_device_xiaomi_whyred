85b4853b0b0c9a1d61bc484c1470ab249d7d2d43
khanfrd <khanfrd94@gmail.com>
2018-08-21 04:02:40 +0530

    whyred: remove camera provider service
    Change-Id: Ia7ae63ac174dbf736c93a8546d4388d537d1c204

--- treble.mk
+++ treble.mk
@@ -21,7 +21,6 @@ PRODUCT_PACKAGES += \
 
 #Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
