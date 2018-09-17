Rahul-sl/whyred

whyred: Add basic USB HAL that reports no status change

treble.mk
@@ -118,7 +118,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.whyred
 
 # Vibrator
 PRODUCT_PACKAGES += \

omnirom/android_device_xiaomi_whyred

Change-Id: I6d130d324753c03ac514c4500c5764bfa46941dc

treble.mk
@@ -79,7 +79,7 @@ PRODUCT_PACKAGES += \
 
 #USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.xiaomi_sdm660
 
 #CAS
 PRODUCT_PACKAGES += \