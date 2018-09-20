1391dad1a5548c0cd5bd50b79f52025213acf02b
khanfrd <khanfrd94@gmail.com>
2018-09-20 18:09:04 +0530

    Revert "whyred: remove camera provider service"
    This reverts commit 85b4853b0b0c9a1d61bc484c1470ab249d7d2d43.

--- treble.mk
+++ treble.mk
@@ -21,6 +21,7 @@ PRODUCT_PACKAGES += \
 
 #Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
