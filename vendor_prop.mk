0cc968fa665c1eaf238124f9702618d069cbdc86
GuaiYiHu <guaiyihu@foxmail.com>
2018-12-11 23:18:03 +0800

    whyred: Add sdm properties as per treble compliance
    Change-Id: I2845f434d7c0266fabd43fb672c505b574273f6d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -106,12 +106,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.gralloc.enable_fb_ubwc=1 \
+    vendor.gralloc.enable_fb_ubwc=1
     debug.sf.recomputecrop=0 \
     debug.sf.hw=1 \
     persist.hwc.enable_vds=1 \
     ro.opengles.version=196610 \
     ro.sf.lcd_density=480 \
     sdm.debug.disable_skip_validate=1 \
+    vendor.display.disable_skip_validate=1
     vendor.display.enable_default_color_mode=0
 
 # DPM
